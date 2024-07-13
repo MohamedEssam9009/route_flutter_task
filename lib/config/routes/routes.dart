f930d501382baa9254ae3279cd8de5fcf94d4f94
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-07-13 15:34:09 +0300

    added a routes class

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class AppRoute {
+  static const String productList = "/";
+}
+
+class Routes {
+ 
+
+  static Widget unDefinedRoute() {
+    return const Center(child: Text("Route Not Found"));
+  }
+}
